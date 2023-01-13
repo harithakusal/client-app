a79e82ca8405e99b7bf1ea1227745c03a2d5229a
Haritha Kusal <harithakusal@gmail.com>
2023-01-13 19:55:35 +0530

    proxy added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React, {useEffect, useState} from 'react'
+import axios from 'axios'
 
 function App() {
   
   const[backendData, setBackendData] = useState([{}])
   
   useEffect(() => {
-    fetch("/api").then(
-      response => response.json()
-    ).then(
-      data => {
-        setBackendData(data)
-      }
-    )
+    // fetch("https://node-app.adaptable.app/api").then(
+    //   response => response.json()
+    // ).then(
+    //   data => { 
+    //     console.log(data)
+    //     setBackendData(data)
+    //   }
+    // )
+    const headers = {
+      'Content-Type': 'application/json',
+      "Access-Control-Allow-Origin": "*"
+  };
+    axios.get("https://node-app.adaptable.app/api", {header:headers}).then(data => {
+      setBackendData(data.data)
+    })
+
   }, [])
   
   return (
